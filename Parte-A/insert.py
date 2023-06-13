d9c3c2ceb4e4a41980afaa28dfb29b53c4393793
jaime gonzalez <jaimegonz654@gmail.com>
2023-06-12 22:58:47 -0400

    cree arhivos .py en parte A

---
+++ Parte-A/insert.py
@@ -0,0 +1,5 @@
+print("1. Indica la información del nuevo medio que quieres añadir con el formato siguiente: nombre, ciudad, region, pais, continente, año de creación")
+
+print("2. Indica una URL de una noticia de este medio")
+
+print("3. Indica la expresión XPATH que permite leer el título de la fecha")
